752267ac868cc02f4f9ee31cd1f62280e70c593e
Denys Xavier <denysxavier@cremesp.org.br>
2018-06-13 17:32:47 -0300

    Adicionado teste para valor padrão de tipoPagamento igual a 1

--- tests/InstrucoesDeTituloTest.php
+++ tests/InstrucoesDeTituloTest.php
@@ -219,6 +219,15 @@ class InstrucoesDeTituloTest extends PHPUnit_Framework_TestCase {
      * @author Denys Xavier <equipe@tiexpert.net>
      * @test
      */
+    public function oValorPadraoDaPropriedadeTipoPagamentoEh1() {
+        $obj = new InstrucoesDeTitulo();
+        $this->assertEquals(1, $obj->getTipoPagamento());
+    }
+
+    /**
+     * @author Denys Xavier <equipe@tiexpert.net>
+     * @test
+     */
     public function oArrayExportadoDevePossuirAsMesmasChavesUtilizadasPeloWSdoBanco() {
         $chaveInstrucao = array("TITULO.PC-MULTA", "TITULO.QT-DIAS-MULTA", "TITULO.PC-JURO", "TITULO.TP-DESC", "TITULO.VL-DESC", "TITULO.DT-LIMI-DESC", "TITULO.VL-ABATIMENTO", "TITULO.TP-PROTESTO", "TITULO.QT-DIAS-PROTESTO", "TITULO.QT-DIAS-BAIXA");
 
