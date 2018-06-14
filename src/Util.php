4fa5536f215d617fc84e5cf91978cd459205ee9d
Denys Xavier <denysxavier@cremesp.org.br>
2018-06-14 18:34:48 -0300

    Adicionado método formatarNumero à classe Util

--- src/Util.php
+++ src/Util.php
@@ -64,4 +64,43 @@ class Util {
         return $data;
     }
 
+    /** Formata um número em uma string específica baseada em sua precisão.
+     * 
+     * Este método diferencia-se da função number_format nativa do PHP por não arredondar o número.
+     * 
+     * @param float $numero Número a ser formatado
+     * @param int $precisao Quantidade de casas decimais
+     * @param string $separadorDecimal Caracter usado como separador decimal
+     * @param string $separadorMilhar Caracter usado como separador de milhar
+     */
+    public static function formatarNumero($numero, $precisao = 0, $separadorDecimal = '.', $separadorMilhar = '') {
+        $partes = explode('.', $numero);
+
+        $numeroFormatado = self::agruparMilhares($partes[0], $separadorMilhar);
+
+        if ($precisao > 0) {
+            if (!isset($partes[1])) {
+                $partes[1] = '0';
+            }
+
+            $parteFracionadaFormatada = str_pad($partes[1], $precisao, '0', STR_PAD_RIGHT);
+            $numeroFormatado .= $separadorDecimal . substr($parteFracionadaFormatada, 0, $precisao);
+        }
+
+        return $numeroFormatado;
+    }
+
+    /** Formata um número inteiro como uma string separando grupos de 3 algarismos com algum caracter especificado
+     * 
+     * @param int $numero Número inteiro a ser formatado
+     * @param string $caracterDeAgrupamento Caracter a ser usado para marcar o agrupamento dos número
+     */
+    private static function agruparMilhares($numero, $caracterDeAgrupamento = '') {
+        $algarismosInvertidos = strrev($numero);
+        $grupoAlgarismosInvertidos = str_split($algarismosInvertidos, 3);
+        $numeroFormatadoInvertido = implode($caracterDeAgrupamento, $grupoAlgarismosInvertidos);
+
+        return strrev($numeroFormatadoInvertido);
+    }
+
 }
